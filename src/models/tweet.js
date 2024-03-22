3237d0b778dc0cd4cb72f71339be57278140b161
Gaurav <gauravwagh0506@gmail.com>
2024-03-22 12:52:08 +0530

    converted to ES modules, removed hashtags from tweets

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const tweetSchema = new mongoose.Schema(
   {
@@ -6,17 +6,11 @@ const tweetSchema = new mongoose.Schema(
       type: String,
       required: true,
       max: [250, "Tweet cannot be more than 250 characters"],
-    },
-    hashtags: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Hashtag",
-      },
-    ],
+    }
   },
   { timestamps: true }
 );
 
 const Tweet = mongoose.model("Tweet", tweetSchema);
 
-module.exports = Tweet;
+export default Tweet;
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,4 +1,4 @@
-const { TweetRepository, HashtagRepository } = require('../repositories/index');
+import { TweetRepository, HashtagRepository } from "../repositories/index.js";
 
 class TweetService {
     constructor() {
@@ -10,7 +10,7 @@ class TweetService {
         try {
             const content = data.content;
             let tags = content.match(/#[A-Za-z0-9\-\.\_]+(?:|$)/g);        // to match the hashtag;
-            tags = tags.map((element) => element.substring(1));
+            tags = tags.map((element) => element.substring(1)).map(tag=>tag.toLowerCase());
             const tweet = await this.tweetRepository.create(data);
             const presentTags = await this.hashtagRepository.findByName(tags);
             const presentTagsTitle = presentTags.map(tag => tag.title);
@@ -18,15 +18,11 @@ class TweetService {
             newTags = newTags.map(tag => {
                 return { title: tag, tweets: [tweet.id] }
             });
-            const response = await this.hashtagRepository.bulkCreate(newTags);
+            await this.hashtagRepository.bulkCreate(newTags);
             presentTags.forEach((tag) => {
                 tag.tweets.push(tweet._id);
-                tweet.hashtags.push(tag._id);
                 tag.save();
             });
-            response.forEach((tag)=>{
-                tweet.hashtags.push(tag._id);
-            })
             tweet.save()
             return tweet;
         } catch (error) {
@@ -35,4 +31,4 @@ class TweetService {
     }
 }
 
-module.exports = TweetService;
+export default TweetService;
